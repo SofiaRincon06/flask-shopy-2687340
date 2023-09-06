a381f585f80b4edfd5f1c59efe02078beda3a8b8
santamaria314k <kevinsantamaria625@gmail.com>
2023-09-06 12:14:13 -0500

    nuevo formulario

--- app/products/forms.py
+++ app/products/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField,IntegerField
+from wtforms.validators import InputRequired, NumberRange
+from flask_wtf.file import FileField, FileRequired, FileAllowed 
 #formulario de registro 
 #de nuevo producto
 class NewProductForm(FlaskForm):
-    nombre = StringField("Ingrese nombre: ")
-    precio = StringField("Ingrese precio: ")
+    nombre = StringField(validators=[InputRequired(message="Falta Nombre :(")],
+                    label="Ingrese nombre: ")
+    precio = IntegerField(label= "Ingrese precio: ",
+                          validators =[InputRequired(message="Falta precio mochuelo"),
+                                       NumberRange(message="Precio fuera de rango",
+                                                   min=1000,
+                                                   max=10000)])
+    imagen= FileField("carge imagen del producto",
+                      validators=[
+                          FileRequired(message="Suba una imagen porfa <3"),
+                      FileAllowed(
+                                ["jpg", "png"],
+                                  message="Tipo de archivo incorrecto")])
     submit = SubmitField("Registrar")
--- app/products/routes.py
+++ app/products/routes.py
@@ -1,6 +1,24 @@
 from flask import render_template
 from . import products
+from . forms import NewProductForm
+import app 
+import os 
 
-@products.route('/create')
+@products.route('/create', methods=["GET", "POST"])
 def crear_producto():
-    return render_template('new.html')
+    p= app.models.Producto()
+    form= NewProductForm()
+    
+    if form.validate_on_submit():
+        ##llenar atributo del objeto producto con eñ formulario
+        form.populate_obj(p)
+        #registrar en bases de datos
+        app.db.session.add(p)
+        p.imagen = form.imagen.data.filename
+        app.db.session.commit()
+        #trasladar la imagen cargada a la carpeta app/prodcts/imagenes 
+        archivo= form.imagen.data
+        archivo.save(os.path.abspath(os.getcwd()+'/app/products/imagenes/'+p.imagen))
+        return os.getcwd()
+    return render_template('new.html',
+                           form=form)
